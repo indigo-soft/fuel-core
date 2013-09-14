576379734533c0276b97b7aced09760d2be421a0
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-09-15 00:11:01 +0200

    Removed before/after

--- classes/job.php
+++ classes/job.php
@@ -2,26 +2,52 @@
 
 namespace Fuel\Core;
 
-class Job {
+class JobException extends \FuelException {}
 
-	public function before() {}
+abstract class Job {
 
-	public function run() {}
+	/**
+	 * Job arguments
+	 *
+	 * @var array
+	 */
+	public $args = array();
 
-	public function after() {}
+	/**
+	 * Abstract function extended by children
+	 *
+	 * @return mixed Job return value
+	 */
+	abstract protected function _run();
+
+	/**
+	 * Run the job
+	 *
+	 * @return mixed Job return value
+	 */
+	public function run()
+	{
+		try
+		{
+			return $this->_run();
+		}
+		catch (\Exception $e)
+		{
+			throw new \JobException($->getMessage(), $e->getCode());
+		}
+	}
+
+	public function __construct($args = array())
+	{
+		$this->args = $args;
+	}
 
 	public function __call($method, $arguments)
 	{
 		switch ($method)
 		{
-			case 'setUp':
-				return $this->before();
-				break;
 			case 'perform':
 				return $this->run();
-				break;
-			case 'tearDown':
-				return $this->after();
 				break;
 			default:
 				# code...

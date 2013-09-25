df06052abb828962261f2f9ee2940f6b641e3393
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-09-25 14:14:12 +0200

    HTML removed

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,4 @@
 Autoloader::add_classes(array(
 	'Fuel\\Core\\Job'          => __DIR__ . '/classes/job.php',
 	'Fuel\\Core\\JobException' => __DIR__ . '/classes/job.php',
-	'Html'                     => __DIR__ . '/classes/html.php',
 ));

cb626cae01c5fa00fd2cd2d62777256060f82ded
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-03 13:35:29 +0200

    Version comments

--- bootstrap.php
+++ bootstrap.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Fuel Core package
+ *
+ * @package 	Fuel
+ * @subpackage	Core
+ * @version		1.0
+ * @author 		Márk Sági-Kazár <mark.sagikazar@gmail.com>
+ * @license 	MIT License
+ * @link 		https://github.com/indigo-soft
+ */
 
 Autoloader::add_classes(array(
 	'Fuel\\Core\\Job'          => __DIR__ . '/classes/job.php',

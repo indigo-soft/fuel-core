8fd1aa9ae82c46628832c624e961f084614e1d36
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-19 10:21:29 +0200

    PHP runtime ini values

--- bootstrap.php
+++ bootstrap.php
@@ -13,6 +13,7 @@
 Autoloader::add_classes(array(
 	'Theme'                 => __DIR__ . '/classes/theme.php',
 	'Num'                   => __DIR__ . '/classes/num.php',
+	'Ini'                   => __DIR__ . '/classes/ini.php',
 	'Twig_Indigo_Extension' => __DIR__ . '/classes/twig/indigo/extension.php',
 	'Monolog\\Handler\\ConsoleHandler'         => __DIR__ . '/classes/monolog/handler/console.php',
 	'Monolog\\Formatter\\ContextLineFormatter' => __DIR__ . '/classes/monolog/formatter/context.php',

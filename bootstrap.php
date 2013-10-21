b505b54ee4e699cba1ae4c974246dbe56d7f4515
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-21 23:32:10 +0200

    Twig extension should either moved to base or merged to Parser package

--- bootstrap.php
+++ bootstrap.php
@@ -11,10 +11,13 @@
  */
 
 Autoloader::add_classes(array(
-	'Theme'                 => __DIR__ . '/classes/theme.php',
-	'Num'                   => __DIR__ . '/classes/num.php',
-	'Ini'                   => __DIR__ . '/classes/ini.php',
-	'Twig_Indigo_Extension' => __DIR__ . '/classes/twig/indigo/extension.php',
+	'Theme' => __DIR__ . '/classes/theme.php',
+	'Num'   => __DIR__ . '/classes/num.php',
+	'Ini'   => __DIR__ . '/classes/ini.php',
+
 	'Monolog\\Handler\\ConsoleHandler'         => __DIR__ . '/classes/monolog/handler/console.php',
 	'Monolog\\Formatter\\ContextLineFormatter' => __DIR__ . '/classes/monolog/formatter/context.php',
+
+	// TODO: This should be moved to indigo-base
+	'Twig_Indigo_Extension' => __DIR__ . '/classes/twig/indigo/extension.php',
 ));

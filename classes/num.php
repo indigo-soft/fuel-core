5e06937534b9d4290eea49e9a50cf5aaa8ee26c4
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-22 11:47:57 +0200

    Num format added

--- classes/num.php
+++ classes/num.php
@@ -5,9 +5,14 @@
  */
 class Num extends \Fuel\Core\Num
 {
-	public static function currency($input, $precision = 4)
+	public static function currency($num, $precision = 4)
 	{
-		is_float($input) or $input = floatval($input);
-		return round($input, $precision);
+		is_float($num) or $num = floatval($num);
+		return round($num, $precision);
+	}
+
+	public static function num_format($num, $dec = 0, $dec_point = ',', $thousand_sep = '.')
+	{
+		return number_format($num, $dec, $dec_point, $thousand_sep);
 	}
 }

353c60a1f24bf4e82d7e06caff76685e1778746d
Barta Tamás <barta.tamas.d@gmail.com>
2013-11-10 22:04:07 +0100

    Temporary solution for Gravatar images, and Gravatar in Twig

--- classes/twig/indigo/extension.php
+++ classes/twig/indigo/extension.php
@@ -17,6 +17,8 @@ class Twig_Indigo_Extension extends Twig_Extension
 	{
 		return array(
 			'auth_has_access'    => new Twig_Function_Function('Auth::has_access'),
+			'gravatar'           => new Twig_Function_Function('Gravatar::forge'),
+			'default_img'        => new Twig_Function_Method($this, 'getDefaultImage'),
 		);
 	}
 
@@ -27,4 +29,11 @@ class Twig_Indigo_Extension extends Twig_Extension
 			'md5'      => new Twig_Filter_Function('md5'),
 		);
 	}
+
+	// base_url() ~ 'assets/theme/img/icons/' ~ (model.group_id == 6 ? 'admin' : model.group_id == 1 ? 'user_cancel' : 'user') ~ '.png' | url_encode
+	public function getDefaultImage(\Auth\Model\Auth_User $model)
+	{
+		return urlencode(Uri::create('assets/theme/img/icons/' . ($model->group_id == 6 ? 'admin' : ($model->group_id == 1 ? 'banned' : 'user') ) . '.png'));
+	}
+
 }

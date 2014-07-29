523aa3c2cbad8ce720c9d986aa3fb9a9851d804c
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-07-29 10:01:58 +0200

    Updates tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,13 +1,27 @@
 <?php
 // This is global bootstrap for autoloading
 
-$_SERVER['doc_root']     = __DIR__ . '/../../../../';
-$_SERVER['app_path']     = __DIR__ . '/../../../app/';
-$_SERVER['core_path']    = __DIR__ . '/../../../core/';
-$_SERVER['package_path'] = __DIR__ . '/../../';
-$_SERVER['vendor_path']  = __DIR__ . '/../../../vendor/';
+$fuel = realpath(__DIR__ . '/../../../..');
 
-require_once __DIR__ . '/../../../core/bootstrap_phpunit.php';
+if ($travis = getenv('TRAVIS_BUILD_DIR'))
+{
+	$fuel = '/tmp/fuel';
+}
+
+$_SERVER['doc_root']     = $fuel;
+$_SERVER['app_path']     = '/fuel/app';
+$_SERVER['core_path']    = '/fuel/core';
+$_SERVER['package_path'] = '/fuel/packages';
+$_SERVER['vendor_path']  = '/fuel/vendor';
+
+require_once $fuel . $_SERVER['core_path'] . '/bootstrap_phpunit.php';
+
+if ($travis)
+{
+	$paths = \Config::get('package_paths', []);
+	$paths[] = realpath($travis.'/..');
+	\Config::set('package_paths', $paths);
+}
 
 \Package::load('core');
 \Composer::package('core');

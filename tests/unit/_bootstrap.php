a8cc163c89d9bfb6079fb0dc1ad03e5162428b11
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-08-13 11:35:30 +0200

    Adds Codeception module

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,6 +1,10 @@
 <?php
 // Here you can initialize variables that will be available to your tests
 
+$package = \Codeception\Configuration::projectDir();
+
+\Package::load('core', $package);
+
 require_once __DIR__ . '/stubs/Controllers.php';
 require_once __DIR__ . '/stubs/Facades.php';
 require_once __DIR__ . '/stubs/Helpers.php';

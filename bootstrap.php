c1a11488931c23f1158677ae5428a0e6f10b877f
Sági-Kazár Márk <mark.sagikazar@gmail.com>
2014-09-19 12:44:16 +0200

    Removes fuel image extension

--- bootstrap.php
+++ bootstrap.php
@@ -17,14 +17,6 @@ Autoloader::add_core_namespace('Indigo\\Fuel', true);
  */
 Autoloader::add_classes(array(
 	/**
-	 * Image class extensions
-	 */
-	'Indigo\\Core\\Image'         => __DIR__ . '/classes/image.php',
-	'Indigo\\Core\\Image_Driver'  => __DIR__ . '/classes/image/driver.php',
-	'Indigo\\Core\\Image_Gd'      => __DIR__ . '/classes/image/gd.php',
-	'Indigo\\Core\\Image_Imagick' => __DIR__ . '/classes/image/imagick.php',
-
-	/**
 	 * Core extensions
 	 */
 	'Indigo\\Core\\Theme'  => __DIR__ . '/classes/theme.php',
